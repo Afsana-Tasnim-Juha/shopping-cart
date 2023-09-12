1a8f723f66b254da6a88901f370490ad5edafbbd
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-09-12 23:55:47 +0200

    clear product

--- cart.js
+++ cart.js
@@ -3,5 +3,7 @@ const addProduct = () => {
     const QuantityField = document.getElementById('product-quantity');
     const product = productField.value;
     const quantity = QuantityField.value;
+    productField.value = '';
+    QuantityField.value = '';
     console.log(product, quantity);
 }
