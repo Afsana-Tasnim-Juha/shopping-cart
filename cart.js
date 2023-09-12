120480ac0927cca9859deee09ab2e275369b7351
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-09-12 23:53:51 +0200

    show product and quantity

---
+++ cart.js
@@ -0,0 +1,7 @@
+const addProduct = () => {
+    const productField = document.getElementById('product-name');
+    const QuantityField = document.getElementById('product-quantity');
+    const product = productField.value;
+    const quantity = QuantityField.value;
+    console.log(product, quantity);
+}
