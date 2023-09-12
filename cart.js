233be9e90562e447049922303926a814e21708cd
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-09-13 00:06:15 +0200

    set ul

--- cart.js
+++ cart.js
@@ -3,7 +3,17 @@ const addProduct = () => {
     const QuantityField = document.getElementById('product-quantity');
     const product = productField.value;
     const quantity = QuantityField.value;
+
     productField.value = '';
     QuantityField.value = '';
+
     console.log(product, quantity);
+    displayProduct(product, quantity);
+}
+
+const displayProduct = (product, quantity) => {
+    const ul = document.getElementById('product-container');
+    const li = document.createElement('li');
+    li.innerText = `${product} : ${quantity}`
+    ul.appendChild(li);
 }
